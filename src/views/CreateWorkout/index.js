a46a43f28b97ea973cb57dda2f61ba320561dfb4
aleksi.valta <aleksi.valta@videovisit.mobi>
2020-06-24 01:12:26 +0300

    creates simple list

--- src/views/CreateWorkout/index.js
+++ src/views/CreateWorkout/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, FlatList, SafeAreaView} from 'react-native';
 import styles from './styles.js';
+
+const data = [
+  {
+    id: 0,
+    movement: 'Bench press',
+    variable: 'Normal',
+    sets: [
+      {
+        reps: 10,
+        weight: '60kg',
+      }, {
+        reps: 8,
+        weight: '65kg',
+      }, {
+        reps: 7,
+        weight: '65kg',
+      },
+    ],
+  }, {
+    id: 1,
+    movement: 'Flies',
+    variable: 'Dumbells',
+    sets: [
+      {
+        reps: 12,
+        weight: '10kg',
+      }, {
+        reps: 11,
+        weight: '10kg',
+      }, {
+        reps: 10,
+        weight: '10kg',
+      },
+    ],
+  },
+];
 
 const CreateWorkout = () => {
   return (
-    <View style={styles.container}>
-      <Text>Text</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={({item}) => (
+          <View style={styles.listItem}>
+            <Text>{item.movement}</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
--- src/views/CreateWorkout/styles.js
+++ src/views/CreateWorkout/styles.js
@@ -1,12 +1,17 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 50,
-    alignItems: "center"
+    marginTop: 20,
   },
-  red: {
-    color: 'red',
-  }
+  listItem: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
 });
